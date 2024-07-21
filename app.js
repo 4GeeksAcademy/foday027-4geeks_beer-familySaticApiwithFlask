7ef32c70d34693e1e1fff8a05fbed8316161a2d5
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-07-21 21:18:47 +0000

    beer-familySaticApiwithFlask

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const PORT = 25000;
-const beers = require("./beers");
+const beers = require("./beers.js");
 
 const app = express();
 // console.log(beers);
@@ -9,7 +9,49 @@ app.get("/", (request, response) => {
   response.send("this is my first route");
 });
 
+app.get("/beers", (request, reponse) => {
+  reponse.json(beers)
+})
+
+app.get("/beer/id/:id", (request, response) => {
+  const { id } = request.params;
+  response.json(beers.filter(beer => beer.id === parseInt(id)));
+});
+
+app.get("/beer/name/:name", (request, response) => {
+  const { name } = request.params;
+  response.json(beers.filter(beer => beer.name.toLowerCase() === name.toLowerCase()))
+  console.log(name, beers[0].name)
+});
+
+app.delete("/beer/id/:id", (request, response) => {
+  const {id} = request.params
+  beerToBeDeleted = beers.find(beer=> beer.id === parseInt(id))
+  if(beerToBeDeleted){
+    beers = beers.filter(beer => beer.id != parseInt(id))
+    response.json(beerToBeDeleted)
+  }
+
+})
+
+app.post("/beer", (request, response) => {
+  const newBeer = request.body;
+  const newId = beers.length > 0 ? Math.max(...beers.map(beer=> beer.id)) + 1 : 1;
+  newBeer.id = newId
+  beers.push(newBeer)
+  response.json(newBeer)
+  
+
+})
+
+
 app.listen(PORT, () => {
   console.log(`user connected on port: ${PORT}`);
 });
-``;
+
+
+{
+  "name": "Sample Beer",
+  "type": "Lager",
+  "abv": 5.0
+}
